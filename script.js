981b518924517fe4b47d93abd24afb79cebde9f0
rishi-221b-bakerstreet <abhisheksingh.a97@gmail.com>
2020-06-14 17:31:07 +0530

    Adding My Tip Calculator Files

---
+++ script.js
@@ -0,0 +1,16 @@
+
+        document.getElementById('container').onchange =function(){
+            var bill=Number(document.getElementById('billTotal').value);
+            var tipPercent = document.getElementById('tipInput').value;
+            var split = document.getElementById('splitInput').value;
+            document.getElementById('tipOutput').innerHTML=tipPercent +"%";
+            document.getElementById('splitOutput').innerHTML=split;
+            var tipValue = bill*(tipPercent /100);
+            var newBillEach = (bill+tipValue)/split;
+            var tipEach = tipValue/split;
+            
+            document.getElementById('newBill').innerHTML ="Rs "+newBillEach.toFixed(2);
+            document.getElementById('tipEach').innerHTML="Rs "+tipEach.toFixed(2);
+            
+
+        }
